74b9dd99a346bdf02d819fb1b2dd1571ce794f83
Stephen Brown II <Stephen.Brown2@gmail.com>
2015-11-25 15:01:58 -0600

    i3lock: cache/parse the whole json info file with jshon, add title

--- i3lock/.local/bin/randxkcd.sh
+++ i3lock/.local/bin/randxkcd.sh
@@ -3,47 +3,46 @@
 # EDIT THIS TO YOUR LIKING. It's the path to the folder containing the cache of comics. Duh
 CACHE=/tmp/xkcd
 
-function fetchurl() {
-   # This function is just in case you haven't got curl.
-   # It could be replaced by wget, /dev/tcp or something like that
-   curl -s "$1" ||
-      { echo "Network error. Fix it" >&2; exit 1; } # Error handling
+function getinfo() {
+  [[ ! -f ${CACHE}/${1}.json ]] && curl -s "https://xkcd.com/$1/info.0.json" -o ${CACHE}/${1}.json
 }
 
 function lastnum() {
-   # Get the number of the latest comic
-   fetchurl 'http://xkcd.com/' |
-      awk '/link to this comic: http:\/\/xkcd.com\// { split(substr($6,17),n,"/"); print n[1] }'
+  # Get the number of the latest comic
+  curl -s 'https://xkcd.com/info.0.json' | jshon -e num
+}
+
+function titletext() {
+   cat ${CACHE}/${1}.json | jshon -Q -e 'title'
 }
 
 function alttext() {
-  if [[ -f "${CACHE}/${1}_alt.txt" ]]; then
-    cat "${CACHE}/${1}_alt.txt"
-  else
-    fetchurl "https://xkcd.com/$1/info.0.json" | jshon -Q -e 'alt' | cleantext | tee "${CACHE}/${1}_alt.txt"
-  fi
+  cat ${CACHE}/${1}.json | jshon -Q -e 'alt' | cleantext
 }
 
 function cleantext() {
   read a;
-  echo "$a" | sed 's/(f)u(ck)/$1*$2/ig'
+  echo "$a" | sed 's/(f)u(ck)/$1-$2/ig'
 }
 
 function imgurl() {
-   fetchurl "https://xkcd.com/$1/info.0.json" | jshon -Q -e 'img' -u
+   cat ${CACHE}/${1}.json | jshon -Q -e 'img' -u
 }
 
 # Get a random number between 1 and Number_Of_Comics
 # Using the modulo operator has the possibility of
 # yielding 0, so we subtract one from Number_Of_Comics,
 # and add it again at the end
-random=$(( ( RANDOM % ( `lastnum` - 1  ) ) + 1))
+random=404
+until [[ ! -f ${CACHE}/${random}.ban ]]; do
+  random=$(( ( RANDOM % ( `lastnum` - 1  ) ) + 1))
+done
 
 [[ "$1" == "--xkcd-true-behavior" ]] && random=4
-
-# Fetch the random html document, find the img tag, get the url
-# (it's the text between the quotes marks, hence the -F to awk)
+# Fetch the random json, parse its goodness
+getinfo ${random}
 image=$(imgurl ${random})
+title=$(titletext ${random})
 alt=$(alttext ${random})
 
 [[ ! -d "$CACHE" ]] && mkdir -p "$CACHE"
@@ -56,9 +55,13 @@ cd "$CACHE" ||
 # If the picture already has been downloaded, the wget command simply does
 # nothing. Easy as 3.14...
 wget --quiet --continue "$image" -O ${random}_$(basename $image) ||
-   { echo "An error occured while downloading $image" >&2; exit 1; } # Still error handling
+   { echo "An error occured while downloading $image" >&2;
+     rm -f ${CACHE}/${random}*;
+     touch ${CACHE}/${random}.ban;
+     exit 1; } # Still error handling
 
 # Now all that's left to do is to print the path to the comic
 echo "filename=${CACHE}/${random}_$(basename "$image")"
 echo "alttext=${alt}"
+echo "title=${title}"
 echo "number=${random}"
